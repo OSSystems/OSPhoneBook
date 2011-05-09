41e597f9478bbf516427c061063b3f51e760d039
Lucas Dutra Nunes <ldnunes@ossystems.com.br>
2011-05-09 18:56:25 -0300

    Added initial schema

---
+++ db/migrate/20110509214523_add_initial_tables.rb
@@ -0,0 +1,36 @@
+class AddInitialTables < ActiveRecord::Migration
+  def self.up
+    create_table :companies do |t|
+      t.string :name
+      t.timestamps
+    end
+
+    create_table :contacts do |t|
+      t.string :name
+      t.text :comments
+      t.references :company
+      t.timestamps
+    end
+
+    create_table :phone_numbers do |t|
+      t.string :number
+      t.integer :type
+      t.references :contact
+      t.timestamps
+    end
+
+    create_table :tags do |t|
+      t.string :name
+      t.timestamps
+    end
+
+    create_table :contacts_tags do |t|
+      t.references :contact, :tag
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_tables :contacts_tags, :tags, :phone_numbers, :contacts, :companies
+  end
+end
