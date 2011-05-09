d830dbab4034f64335b9fa6911ad4ccc47f64790
Lucas Dutra Nunes <ldnunes@ossystems.com.br>
2011-05-09 18:43:57 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OsPhoneBook::Application.config.secret_token = 'd3d3d3ecb598fc57aeeb48a794a0d47c8cecdd0e4e75fc1c7a7ad8889568c2c311b8b1b78c386131947da88e170f4bbbf4691c8a0f94cadd6bb2dbd9c2e60a99'
