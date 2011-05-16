0dd57ae7588b06386ca84a8833aa4e833718e57e
Lucas Dutra Nunes <ldnunes@ossystems.com.br>
2011-05-16 10:51:24 -0300

    Corrected javascript

--- public/javascripts/autocomplete.js
+++ public/javascripts/autocomplete.js
@@ -326,14 +326,14 @@ Autocomplete.prototype = {
 
 	div.insert({bottom: this.highlight(value)});
 
-        var company_text = this.data[index][1]
+        var company_text = this.data[index][1];
         if (!company_text.empty()) {
 	    var company_div = new Element('div', {'class': 'company', 'title': 'Company'});
             company_div.insert({top: this.highlight(company_text)});
             div.insert({bottom: company_div});
         }
 
-        var tags_text = this.data[index][2].join(", ")
+        var tags_text = this.data[index][2].join(", ");
         if (!tags_text.empty()) {
 	    var tags_div = new Element('div', {'class': 'tags', 'title': 'Related tags'});
             tags_div.insert({top: this.highlight(tags_text)});
