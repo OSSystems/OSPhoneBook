f88b568c65a2b0265ec6551253555d46854b0c3a
Lucas Dutra Nunes <ldnunes@ossystems.com.br>
2011-05-17 15:45:27 -0300

    Added new contact tab

--- app/helpers/tabs_helper.rb
+++ app/helpers/tabs_helper.rb
@@ -2,6 +2,7 @@ module TabsHelper
   def render_tabs
     user_tabs = []
     user_tabs << ["Search", root_path]
+    user_tabs << ["New contact", new_contact_path]
 
     render :partial => "tabs/tabs", :locals => {:tabs => user_tabs}
   end
