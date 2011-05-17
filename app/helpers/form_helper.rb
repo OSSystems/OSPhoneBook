328e323c95b72979c4a5e73ed49d8fedc784b343
Lucas Dutra Nunes <ldnunes@ossystems.com.br>
2011-05-17 10:01:03 -0300

    Do not send invalid attributes to new object tags

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -5,11 +5,11 @@ module FormHelper
 
   def link_add_child(name, f, association, options)
     new_object = f.object.class.reflect_on_association(association).klass.new
-    options[:partial] ||= association.to_s.singularize + "_childs"
-    options[:container] ||= 'this'
+    partial = (options[:partial] or association.to_s.singularize + "_childs")
+    container = (options[:container] or 'this')
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(:partial => options[:partial], :locals => {:f => builder})
     end
-    link_to_function(name, "add_childs('#{options[:container]}', \"#{association}\", \"#{escape_javascript(fields)}\")", options)
+    link_to_function(name, "add_childs('#{container}', \"#{association}\", \"#{escape_javascript(fields)}\")", options[:html])
   end
 end
