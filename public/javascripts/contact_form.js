26d919fb412529bb3822bce461c80b359d7a83b2
Lucas Dutra Nunes <ldnunes@ossystems.com.br>
2011-05-18 10:01:32 -0300

    Company search javascript improvements

--- public/javascripts/contact_form.js
+++ public/javascripts/contact_form.js
@@ -3,7 +3,21 @@ Event.observe(document, 'dom:loaded', function() {
         serviceUrl: '/company_search',
         minChars: 1,
         maxHeight: 400,
-        width: 'auto',
-        deferRequestBy: 200
+        width: 390,
+        deferRequestBy: 200,
+        onSelect: function(value, data) {
+            var element = $('company_search_field');
+            var form = element.form;
+            var next_id = 0;
+            for (var i = 0; i < form.elements.length; i++) {
+                if (form.elements[i] == element) {
+                    next_id = i + 1;
+                }
+            }
+            var next_element = form.elements[next_id];
+            if (next_element) {
+                next_element.focus();
+            }
+        }
     });
 });
