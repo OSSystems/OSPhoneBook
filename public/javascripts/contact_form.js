f32109714c4b18afea61428e2a754e029d819c47
Lucas Dutra Nunes <ldnunes@ossystems.com.br>
2011-05-26 18:24:36 -0300

    Fix bugs on contact forms

--- public/javascripts/contact_form.js
+++ public/javascripts/contact_form.js
@@ -18,7 +18,7 @@ Event.observe(document, 'dom:loaded', function() {
         width: 390,
         deferRequestBy: 200,
         onSelect: function(value, data) {
-            $('company_search_field').value = "";
+            $('add_tag').value = "";
             updateFormTags(data);
         }
     });
