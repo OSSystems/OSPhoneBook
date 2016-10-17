ffd0096170f1c9326485f76b1b253a2f6a443f06
Lucas Dutra Nunes <ldnunes@ossystems.com.br>
2016-10-17 10:29:40 -0200

    Docker: remove pid file when (re)starting the web container
    Signed-off-by: Lucas Dutra Nunes <ldnunes@ossystems.com.br>

--- bin/start-server.sh
+++ bin/start-server.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 set -e
+
+# Delete pid file if we are (re)starting the container. This is necessary when
+# the container was shut down ungracefully, and left the pid file. If the
+# container is then restarted it fails shortly after as the pid file is still
+# present. Simply deleting it when starting solves the problem.
+rm -f /usr/src/app/tmp/pids/server.pid
+
 exec "rails" "server" "-b" "0.0.0.0"
